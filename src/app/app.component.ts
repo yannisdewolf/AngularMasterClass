6b4c30b3e1941101d2df025890cfa600e8c663a3
yannis <yannisdewolf@gmail.com>
2017-08-15 15:00:19 +0200

    angular properties bind to DOM representation
    ==> td [colspan]="colspan" werkt niet want colspan is geen onderdeel van DOM representation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <img [src]="imageurl"/>
+    <table [border]="borderthickness">
+      <tr>
+        <td [attr.colspan]="colspan">&nbsp;</td>
+      </tr>
+      <tr>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+      </tr>
+    </table>
+  `
+ ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  borderthickness = 3;
+
+  colspan = 3;
+
+
+  imageurl = 'http://lorempixel.com/400/200';
+
   title = 'app';
 }
