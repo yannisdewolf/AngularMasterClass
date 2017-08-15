3ea8b4969c123d1a0934d9ec9e991101c61895e1
yannis <yannisdewolf@gmail.com>
2017-08-15 19:25:12 +0200

    built-in pipes (date, number, currency)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <input type='text' (keyup.enter)='email = $event.target.value; onKeyUp()' [value]='email'>
-
-
-    <input [(ngModel)]='email' (keyup.enter)='onKeyUp()'>
+   {{ course.title | uppercase |lowercase}} <br>
+    {{course.price | currency:'EUR':true:'1.2-2'}} <br>
+    {{course.students | number}} <br>
+    {{course.rating | number:'1.2-5'}}  (minstens 1 geheel getal, min2 decimalen, maximum 5 decimalen <br>
+    {{course.relaseDate | date}} (zie datePipe class)
+    
   `
   ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  email = 'me@example.com';
+  course = {
+    title: 'the cmplete guide',
+    rating: 14.9766,
+    students: 12345,
+    price: 10.9688,
+    relaseDate: new Date(2016, 3, 1)
+  };
 
-  onKeyUp() {
-    console.log(this.email);
-  }
 
 }
 
