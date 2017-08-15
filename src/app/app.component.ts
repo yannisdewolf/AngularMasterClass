67daa1c3ce3eab85f092fed5eff2b77b58b98899
yannis <yannisdewolf@gmail.com>
2017-08-15 15:34:50 +0200

    style binding (style.backgroundColor) attribute biding (class.active)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <img [src]="imageurl"/>
-    <table [border]="borderthickness">
+    <div [style.backgroundColor]="isActive? 'blue':'white'">groen</div>
+
+    
+    <button class="btn btn-primary" [class.active]="isActive"> klik</button>
+    <button class="btn btn-primary" [class.active]="isActive" disabled>active disabled</button>
+    <button class="btn btn-primary" [class.active]="!isActive">klik</button>
+    <button class="btn btn-primary" [class.active]="!isActive" disabled>!active disabled</button>        <img
+      [src]="imageurl"/>
+    <table>
       <tr>
         <td [attr.colspan]="colspan">&nbsp;</td>
       </tr>
       <tr>
-      <td>1</td>
-      <td>2</td>
-      <td>3</td>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
       </tr>
-    </table>
-  `
- ,
+    </table>    `
+  ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  groen = 'green';
+
+  isActive = true;
 
   borderthickness = 3;
 
