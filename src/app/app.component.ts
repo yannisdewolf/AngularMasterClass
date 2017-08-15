c577cb5c590ffe22a762b06e91ad549339905cbd
yannis <yannisdewolf@gmail.com>
2017-08-15 16:52:18 +0200

    template variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,39 +3,18 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div [style.backgroundColor]="isActive? 'blue':'white'">groen</div>
-
-    
-    <button class="btn btn-primary" [class.active]="isActive"> klik</button>
-    <button class="btn btn-primary" [class.active]="isActive" disabled>active disabled</button>
-    <button class="btn btn-primary" [class.active]="!isActive">klik</button>
-    <button class="btn btn-primary" [class.active]="!isActive" disabled>!active disabled</button>        <img
-      [src]="imageurl"/>
-    <table>
-      <tr>
-        <td [attr.colspan]="colspan">&nbsp;</td>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-      </tr>
-    </table>    `
+    <input #email type='text' (keyup.enter)='onKeyUp(email.value)'>
+  `
   ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  groen = 'green';
 
-  isActive = true;
+  onKeyUp(email) {
+    console.log(email);
+  }
 
-  borderthickness = 3;
-
-  colspan = 3;
-
-
-  imageurl = 'http://lorempixel.com/400/200';
-
-  title = 'app';
 }
+
+
