2830c9126006f3ff30e6d5c264645f7d7fbb90fe
yannis <yannisdewolf@gmail.com>
2017-08-26 21:39:16 +0200

    sectie 7: parse form data in component

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -9,6 +10,13 @@ export class ContactFormComponent {
 
   log(x) {
     console.log(x)
+  }
+
+  submit(f: NgForm) {
+    let form = f.form;
+    let firstName = form.controls.firstName;
+    console.log('firstName ', f.value.firstName);
+    console.log(f);
   }
 
 }
