8c65c20ca7e0ec0f908d7b3c40ca4424912f7392
yannis <yannisdewolf@gmail.com>
2017-08-26 11:09:31 +0200

    sectie 6: ngfor en exported values van ngfor + modify list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,28 @@ export class AppComponent {
 
   viewMode = 'somethingElse';
 
+  courses = [
+    {
+      id: 1,
+      name: 'course1'
+    },
+    {
+      id: 2,
+      name: 'course2'
+    },
+    {
+      id: 3,
+      name: 'course3'
+    }
+  ];
+
+  onAdd() {
+    this.courses.push({id: 4, name: 'course 4'});
+  }
+
+  onChange(course) {
+    course.name = 'UPDATED';
+  }
 }
 
 
