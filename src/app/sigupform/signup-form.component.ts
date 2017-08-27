6d0c8d1c03785fc263eb54ac3c57b1812e53fe15
yannis <yannisdewolf@gmail.com>
2017-08-27 16:09:31 +0200

    sectie 8: multiple validations on single field

--- src/app/sigupform/signup-form.component.ts
+++ src/app/sigupform/signup-form.component.ts
@@ -9,7 +9,11 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class SignupFormComponent {
 
   form = new FormGroup({
-    username: new FormControl('', Validators.required),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(15)
+    ]),
     password: new FormControl('', Validators.required)
   });
 
