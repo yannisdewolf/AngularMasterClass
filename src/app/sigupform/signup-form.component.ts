f71ea5c580ddce9b1e7a9c40988761f16eb724c5
yannis <yannisdewolf@gmail.com>
2017-08-27 16:20:34 +0200

    sectie 8: custom validation error

--- src/app/sigupform/signup-form.component.ts
+++ src/app/sigupform/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UsernameValidators} from './username.validators';
 
 @Component({
   selector: 'app-signup-form',
@@ -12,7 +13,8 @@ export class SignupFormComponent {
     username: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
-      Validators.maxLength(15)
+      Validators.maxLength(15),
+      UsernameValidators.cannotContainSpace
     ]),
     password: new FormControl('', Validators.required)
   });
---
+++ src/app/sigupform/username.validators.ts
@@ -0,0 +1,15 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+export class UsernameValidators {
+
+  static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
+    if ((control.value as string).indexOf(' ') >= 0 ) {
+      return {
+        cannotContainSpace: true
+      };
+    }
+
+    return null;
+  }
+
+}
