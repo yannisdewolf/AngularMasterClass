ea5855f1672f4e3ce651abc27ac55bae6431753e
yannis <yannisdewolf@gmail.com>
2017-08-27 16:25:56 +0200

    sectie 8: simulate asynchronous call for validation

--- src/app/sigupform/username.validators.ts
+++ src/app/sigupform/username.validators.ts
@@ -1,15 +1,32 @@
-import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+import {AbstractControl, ValidationErrors} from '@angular/forms';
 
 export class UsernameValidators {
 
-  static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
-    if ((control.value as string).indexOf(' ') >= 0 ) {
+  static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+    if ((control.value as string).indexOf(' ') >= 0) {
       return {
         cannotContainSpace: true
       };
     }
 
     return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+
+    //async operation, simulate call to server
+    setTimeout(() => {
+      if (control.value === 'yannis') {
+        return {
+          shouldBeUnique: true
+        };
+      }
+      return null;
+    }, 2000);
+
+
+    return null;
+
   }
 
 }
