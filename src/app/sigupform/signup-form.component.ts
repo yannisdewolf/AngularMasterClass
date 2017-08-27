b222bcc939c79f488e3c5d858153123242bdfcff
yannis <yannisdewolf@gmail.com>
2017-08-27 16:05:58 +0200

    sectie 8: validation on username

--- src/app/sigupform/signup-form.component.ts
+++ src/app/sigupform/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
@@ -9,8 +9,14 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class SignupFormComponent {
 
   form = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
 
+  get username(){
+    return this.form.get('username');
+  }
+
 }
+
+
