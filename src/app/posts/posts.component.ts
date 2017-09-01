d25d456f7085f9d400c1d36261e831e987f5dd3b
yannis <yannisdewolf@gmail.com>
2017-09-01 14:03:41 +0200

    sectie 9: error handling from Observables

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -10,15 +10,21 @@ export class PostsComponent implements OnInit {
 
   posts: any[];
 
+  errorOccured: boolean;
 
   constructor(private service: PostService) {
   }
 
   ngOnInit(): void {
+    this.errorOccured = false;
     this.service.getPosts()
-      .subscribe(resp => {
-        this.posts = resp.json();
-      });
+      .subscribe(
+        response => {
+          this.posts = response.json();
+        },
+        error => {
+          this.errorOccured = true;
+        });
   }
 
   createPost(input: HTMLInputElement) {
@@ -26,30 +32,55 @@ export class PostsComponent implements OnInit {
     input.value = '';
 
     this.service.createPost(post)
-      .subscribe(response => {
-        post['id'] = response.json().id;
-        this.posts.splice(0, 0, post);
-        console.log('response after post ', response.json());
-      }
-    );
+      .subscribe(
+        response => {
+          post['id'] = response.json().id;
+          this.posts.splice(0, 0, post);
+          console.log('response after post ', response.json());
+        },
+        (error: Response) => {
+          if(error.status === 400) {
+            //this.form.setErrors(error.json()); //set errors on form
+          } else {
+            console.log('error occured', error);
+          }
+        }
+      );
   }
 
   updatePost(post: any) {
     // update few properties instead of full object
     this.service.markAsRead(post)
-      .subscribe(response => {
-        const index = this.posts.indexOf(post);
-        this.posts[index]['isRead'] = true;
-      });
+      .subscribe(
+        response => {
+          const index = this.posts.indexOf(post);
+          this.posts[index]['isRead'] = true;
+        },
+        error => {
+          console.log('error occured', error)
+        });
 
-    // this.http.put(this.url, JSON.stringify(post));
   }
 
   deletePost(post: any) {
-    this.service.deletePost(post.id)
-      .subscribe(response => {
-        const index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      });
+    //this.service.deletePost(post.id)
+    this.service.deletePost(345)
+      .subscribe(
+        response => {
+          const index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+        },
+        (error: Response) => {
+          if(error.status === 404) {
+            alert('This post has already been deleted');
+          } else {
+            this.errorOccured = true;
+            console.log(error);
+          }
+        });
+  }
+
+  dismissError() {
+    this.errorOccured = false;
   }
 }
