564c7e70ebb8ff545774cf48483ea1356056b8ab
yannis <yannisdewolf@gmail.com>
2017-09-08 16:49:18 +0200

    sectie 10: programmatic navigation with Router.navigate

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -8,7 +8,13 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
+  }
+
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest'}
+    });
   }
 
   ngOnInit() {
