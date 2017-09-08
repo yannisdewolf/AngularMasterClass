07e93452e4467c83f07ae19ee82285be113a8f21
yannis <yannisdewolf@gmail.com>
2017-09-08 16:10:57 +0200

    sectie 10: Route parameters are observables when loading different content on same page without quitting the page.

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -12,11 +12,19 @@ export class GithubProfileComponent implements OnInit {
   }
 
   ngOnInit() {
+
+
+    // subscribe op route.paramMap wanneer user op zelfde pagina kan blijven
     this.route.paramMap
       .subscribe(params => {
         let id = +params.get('id');
-        console.log(id);
+        console.log('id as object from observable paramMap: ' + id);
       });
+
+    //wanneer user op andere pagina komt, en nadien terug op deze pagina komt, kan je snapshot gebruiken:
+
+    let id2 = this.route.snapshot.paramMap.get('id');
+    console.log('id as object from snapshot: ' + id2);
   }
 
 }
